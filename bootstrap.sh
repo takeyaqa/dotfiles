8c978dda1dc87bedc4dba6c776e64d9ed0603232
Takeya <takeya0x86@gmail.com>
2015-08-15 23:58:00 +0900

    init

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DOTFILES_ROOT=`pwd`
+
+ln -s $DOTFILES_ROOT/.ctags $HOME/.ctags
+ln -s $DOTFILES_ROOT/.gemrc $HOME/.gemrc
+ln -s $DOTFILES_ROOT/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILES_ROOT/.gitignore_global $HOME/.gitignore_global
+ln -s $DOTFILES_ROOT/.zshrc $HOME/.zshrc
