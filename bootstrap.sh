1cb1fbc6fb4136592274849e7d7e8b4dfc023f06
Takeshi Kishi <takeya0x86@gmail.com>
2020-05-05 11:37:15 +0900

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,4 @@ ln -s $DOTFILES_ROOT/.gitconfig $HOME/.gitconfig
 ln -s $DOTFILES_ROOT/.gitignore_global $HOME/.gitignore_global
 ln -s $DOTFILES_ROOT/.zshenv $HOME/.zshenv
 ln -s $DOTFILES_ROOT/.zshrc $HOME/.zshrc
-ln -s $DOTFILES_ROOT/.Brewfile $HOME/.Brewfile
+ln -s $DOTFILES_ROOT/Brewfile $HOME/Brewfile
