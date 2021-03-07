d15984fedccfceb6c81e7672405a23a77e3ad294
Takeshi Kishi <takeya0x86@gmail.com>
2021-03-07 13:02:48 +0900

    delete unused file

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,3 @@ ln -s $DOTFILES_ROOT/.gitconfig $HOME/.gitconfig
 ln -s $DOTFILES_ROOT/.gitignore_global $HOME/.gitignore_global
 ln -s $DOTFILES_ROOT/.zshenv $HOME/.zshenv
 ln -s $DOTFILES_ROOT/.zshrc $HOME/.zshrc
-ln -s $DOTFILES_ROOT/Brewfile $HOME/Brewfile
