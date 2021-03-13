3227967e163bbf991fb82866a816e69d62fb05eb
Takeshi Kishi <takeya0x86@gmail.com>
2021-03-14 01:04:22 +0900

    update zsh settings

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DOTFILES_ROOT=`pwd`
-
-ln -s $DOTFILES_ROOT/.gemrc $HOME/.gemrc
-ln -s $DOTFILES_ROOT/.gitconfig $HOME/.gitconfig
-ln -s $DOTFILES_ROOT/.gitignore_global $HOME/.gitignore_global
-ln -s $DOTFILES_ROOT/.zshenv $HOME/.zshenv
-ln -s $DOTFILES_ROOT/.zshrc $HOME/.zshrc
