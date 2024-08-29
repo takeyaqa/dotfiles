f11814cd95d472b378ff42d44eec0bcc761d251a
Takeshi Kishi <takeyaqa@gmail.com>
2024-08-29 13:28:13 +0900

    rename directory

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 ln -s $PWD/dot_zshenv $HOME/.zshenv
 ln -s $PWD/dot_zprofile $HOME/.zprofile
 ln -s $PWD/dot_zshrc $HOME/.zshrc
-ln -s $PWD/private_dot_config/git/config $HOME/.config/git/config
+ln -s $PWD/dot_config/git/config $HOME/.config/git/config
